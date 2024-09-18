8efd150ef3d25c165f13d19f3b51a3398cea85e8
HanniizLara <nattapat_toon@hotmail.com>
2024-09-18 16:54:40 +0700

    feat: init nextui table example

--- app/components/services/utils.ts
+++ app/components/services/utils.ts
@@ -1,3 +1,7 @@
 export function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
+
+export function capitalize(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
---
+++ app/routes/item-logs.tsx
@@ -0,0 +1,7 @@
+export default function ItemLogs() {
+  return (
+    <div className="bg-gray-950 h-screen w-full">
+      <h1>Item Logs</h1>
+    </div>
+  );
+}
