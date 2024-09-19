b9f93ec09e8ba51ca3f471d8870097b98a352618
HanniizLara <nattapat_toon@hotmail.com>
2024-09-19 17:55:41 +0700

    refactor: make the top content compoennt

--- app/components/item-logs/table.tsx
+++ app/components/item-logs/table.tsx
@@ -24,6 +24,7 @@ import { ChevronDownIcon } from "../icons/chevron-down-icon";
 import { capitalize, currencyFormatterr } from "../services/utils";
 import { PlusIcon } from "../icons/plus-icon";
 import { ItemLog, TransactionStatus } from "../services/item-logs.service";
+import { TopContent } from "./top-content";
 const INITIAL_VISIBLE_COLUMNS = [
   "item_name",
   "final_price",
@@ -215,76 +216,18 @@ export default function ItemLogsTable({ itemLogs }: { itemLogs: ItemLog[] }) {
     setPage(1);
   }, []);
 
-  const topContent = React.useMemo(() => {
-    return (
-      <div className="flex flex-col gap-4">
-        <div className="flex justify-between gap-3 items-end">
-          <Input
-            isClearable
-            className="w-full sm:max-w-[44%]"
-            placeholder="Search by name..."
-            startContent={<SearchIcon />}
-            value={filterValue}
-            onClear={() => onClear()}
-            onValueChange={onSearchChange}
-          />
-          <div className="flex gap-3">
-            <Dropdown>
-              <DropdownTrigger className="hidden sm:flex">
-                <Button
-                  endContent={<ChevronDownIcon className="text-small" />}
-                  variant="flat"
-                >
-                  Columns
-                </Button>
-              </DropdownTrigger>
-              <DropdownMenu
-                disallowEmptySelection
-                aria-label="Table Columns"
-                closeOnSelect={false}
-                selectedKeys={visibleColumns}
-                selectionMode="multiple"
-                onSelectionChange={setVisibleColumns}
-              >
-                {columns.map((column) => (
-                  <DropdownItem key={column.uid} className="capitalize">
-                    {capitalize(column.name)}
-                  </DropdownItem>
-                ))}
-              </DropdownMenu>
-            </Dropdown>
-            <Button color="primary" endContent={<PlusIcon />}>
-              Add New
-            </Button>
-          </div>
-        </div>
-        <div className="flex justify-between items-center">
-          <span className="text-default-400 text-small">
-            Total {itemLogs.length} items
-          </span>
-          <label className="flex items-center text-default-400 text-small">
-            Rows per page:
-            <select
-              className="bg-transparent outline-none text-default-400 text-small"
-              onChange={onRowsPerPageChange}
-            >
-              <option value="5">5</option>
-              <option value="10">10</option>
-              <option value="15">15</option>
-            </select>
-          </label>
-        </div>
-      </div>
-    );
-  }, [
+  const topContent = TopContent({
     filterValue,
-    statusFilter,
-    visibleColumns,
+    onClear,
     onSearchChange,
+    visibleColumns,
+    setVisibleColumns,
     onRowsPerPageChange,
-    itemLogs.length,
+    itemLogs,
+    columns,
+    statusFilter,
     hasSearchFilter,
-  ]);
+  });
 
   const bottomContent = React.useMemo(() => {
     return (
